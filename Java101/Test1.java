1e542109c4610ceb9e89d2e9188ff75c26099c64
erdemyagsagan <rdemy153@gmail.com>
2022-03-11 23:32:05 +0300

    first commit

---
+++ Java101/Test1.java
@@ -0,0 +1,26 @@
+package Java101;
+import java.util.Scanner;
+
+public class Test1{
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+    int mat,fizik,kimya,inkilap,turkce;
+System.out.print("Matematik notunuzu giriniz: ");
+mat = input.nextInt();
+System.out.print("Fizik notunuzu giriniz: ");
+fizik = input.nextInt();
+System.out.print("Kimya notunuzu giriniz: ");
+kimya   = input.nextInt();
+System.out.print("Inkilap notunuzu giriniz: ");
+inkilap = input.nextInt();
+System.out.print("Turkce notunuzu giriniz: ");
+turkce = input.nextInt();
+int toplam = (mat + fizik + kimya + inkilap + turkce);
+double ortalama = (toplam / 5 );
+System.out.println("Ortalamaniz: " + ortalama);
+
+String str = (ortalama<60)? "Sinifta kaldi." : "Sinifi gecti";
+System.out.println(str);
+input.close();
+}
+}
