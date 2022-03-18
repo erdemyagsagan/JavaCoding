6d60ba78ce963f44fdc6ed486aa49d1b6505dae6
erdemyagsagan <rdemy153@gmail.com>
2022-03-19 00:53:51 +0300

    uslusayi

---
+++ Java101/UsluSayi.java
@@ -0,0 +1,19 @@
+package Java101;
+import java.util.Scanner;
+
+public class UsluSayi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+int sayi,us,i,toplam=1;
+System.out.println("Hesaplamak istedigniz uslu sayi islemini giriniz: ");
+sayi = input.nextInt();
+System.out.print(sayi+" ^ ");
+us = input.nextInt();
+for(i = 1;i <= us; i++ ){
+    toplam = toplam * sayi;
+    
+}System.out.println(toplam);
+
+        input.close();
+    }
+}
